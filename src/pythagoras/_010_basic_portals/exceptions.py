0477108da8c2c16ddbdc426525234cd12d6d005d
Vlad Pavlov <vlpavlov@ieee.org>
2025-06-22 02:20:02 -0500

    CLN: delete unused code

--- src/pythagoras/_010_basic_portals/exceptions.py
+++ src/pythagoras/_010_basic_portals/exceptions.py
@@ -2,10 +2,6 @@ class PythagorasException(Exception):
     def __init__(self, message):
         self.message = message
 
-class NotPicklableObject(PythagorasException):
-    def __init__(self, message):
-        PythagorasException.__init__(self, message)
-
 
 class NonCompliantFunction(PythagorasException):
     def __init__(self, message):
