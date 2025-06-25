b3a1de10f2f8ff9d1d0ee0ad8208861024e6ff2d
Vlad Pavlov <vlpavlov@ieee.org>
2025-06-24 23:55:27 -0500

    TST: add a consistency check for PostInitMeta.

--- src/pythagoras/_010_basic_portals/post_init_metaclass.py
+++ src/pythagoras/_010_basic_portals/post_init_metaclass.py
@@ -6,4 +6,7 @@ class PostInitMeta(ABCMeta):
     def __call__(self, *args, **kwargs):
         instance = super().__call__(*args, **kwargs)
         instance._post_init_hook()
+        #TODO: remove/improve this check at some point in the future
+        if hasattr(instance, '_visited_portals'):
+            assert len(instance._visited_portals) == 0
         return instance
