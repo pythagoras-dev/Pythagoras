277bdf5e4be51eefaf27a401b3fc9644192f2a3a
Vlad Pavlov <vlpavlov@ieee.org>
2025-07-05 18:42:52 -0500

    DOC: polish docstrings

--- src/pythagoras/__init__.py
+++ src/pythagoras/__init__.py
@@ -10,8 +10,12 @@ we replace expensive compute with cheap storage.
 - High performance: 'compute once, reuse forever' strategy
 significanty accelerates long-running workflows.
 
-Pythagoras is able to affer these benefits as it's the first framework
+Pythagoras is able to offer these benefits as it's the first framework
 to fully implement the Functional Programming 2.0 paradigm.
+
+Pythagoras excels at optimizing complex, long-running,
+resource-demanding computations. It’s not the best choice for real-time,
+latency-sensitive workflows.
 """
 
 
