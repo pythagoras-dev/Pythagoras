ea45b7a19fe1ee9dd6d007aea71fe850c4c3f857
Vlad Pavlov <vlpavlov@ieee.org>
2025-10-02 21:59:29 -0500

    TST: add parameter tests for all portals

---
+++ tests/_070_protected_code_portals/test_protected_code_portal_params.py
@@ -0,0 +1,20 @@
+import parameterizable
+
+from pythagoras import _PortalTester, ProtectedCodePortal
+
+
+def test_portal(tmpdir):
+    the_dir = tmpdir
+    with _PortalTester():
+
+        portal1 = ProtectedCodePortal(tmpdir.mkdir("edede"))
+        portal1_params = portal1.get_params()
+        portal1_params_json = parameterizable.dumps(portal1)
+
+        portal2 = parameterizable.loads(portal1_params_json)
+        portal2_params = portal2.get_params()
+        portal1_params_json2 = parameterizable.dumps(portal2)
+
+        assert portal1_params == portal2_params
+        assert portal1_params_json == portal1_params_json2
+    
