79054c0dcac350b3e1a72d88160ee47dc078598f
Vlad Pavlov <vlpavlov@ieee.org>
2025-10-02 21:12:05 -0500

    REF: simplify OutputSuppressor using contextlib

--- src/pythagoras/_090_swarming_portals/output_suppressor.py
+++ src/pythagoras/_090_swarming_portals/output_suppressor.py
@@ -1,23 +1,18 @@
 import os
-import sys
+from contextlib import ExitStack, redirect_stderr, redirect_stdout
+
 
 
 class OutputSuppressor:
     """A context manager that suppresses stdout and stderr output."""
-    def __init__(self):
-        pass
 
     def __enter__(self):
         """Redirect stdout and stderr to os.devnull."""
-        self.old_stdout = sys.stdout
-        self.old_stderr = sys.stderr
-        self.devnull = open(os.devnull, 'w')
-        sys.stdout = self.devnull
-        sys.stderr = self.devnull
+        self._stack = ExitStack()
+        devnull = self._stack.enter_context(open(os.devnull, "w"))
+        self._stack.enter_context(redirect_stdout(devnull))
+        self._stack.enter_context(redirect_stderr(devnull))
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.stdout = self.old_stdout
-        sys.stderr = self.old_stderr
-        if self.devnull:
-            self.devnull.close()
+        return self._stack.__exit__(exc_type, exc_val, exc_tb)
