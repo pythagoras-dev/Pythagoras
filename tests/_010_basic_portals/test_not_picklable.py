2fd03a70a89b97489023c197319f957fb1222ac3
Vlad Pavlov <vlpavlov@ieee.org>
2025-10-11 13:56:06 -0500

    TST: improve unit-test for NotPicklable.

--- tests/_010_basic_portals/test_not_picklable.py
+++ tests/_010_basic_portals/test_not_picklable.py
@@ -4,15 +4,6 @@ from pythagoras._010_basic_portals import NotPicklable
 
 import pytest
 
-# def test_not_picklable():
-#     npo = NotPicklable()
-#     with pytest.raises(TypeError, match="NotPicklable cannot be pickled"):
-#         _ = npo.__getstate__()
-#
-#     with pytest.raises(TypeError, match="NotPicklable cannot be unpickled"):
-#         npo.__setstate__({"key": "value"})
-
-
 def test_not_picklable():
     npo = NotPicklable()
 
