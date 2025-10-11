7ac0e538192be17e4ffb9bc711177867f39cad61
Vlad Pavlov <vlpavlov@ieee.org>
2025-10-11 13:55:49 -0500

    TST: improve unit-test for NotPicklable.

--- tests/_010_basic_portals/test_not_picklable.py
+++ tests/_010_basic_portals/test_not_picklable.py
@@ -1,11 +1,36 @@
+import pickle
+
 from pythagoras._010_basic_portals import NotPicklable
 
 import pytest
 
+# def test_not_picklable():
+#     npo = NotPicklable()
+#     with pytest.raises(TypeError, match="NotPicklable cannot be pickled"):
+#         _ = npo.__getstate__()
+#
+#     with pytest.raises(TypeError, match="NotPicklable cannot be unpickled"):
+#         npo.__setstate__({"key": "value"})
+
+
 def test_not_picklable():
     npo = NotPicklable()
-    with pytest.raises(TypeError, match="NotPicklable cannot be pickled"):
-        _ = npo.__getstate__()
 
-    with pytest.raises(TypeError, match="NotPicklable cannot be unpickled"):
+    # Test dunder methods directly
+    with pytest.raises(TypeError):
+        npo.__reduce__()
+    with pytest.raises(TypeError):
+        _ = npo.__getstate__()
+    with pytest.raises(TypeError):
         npo.__setstate__({"key": "value"})
+
+    # Test pickling via pickle.dumps(), which calls __reduce__()
+    with pytest.raises(TypeError):
+        pickle.dumps(npo)
+
+    # Test that a subclass is also not picklable
+    class MyNotPicklable(NotPicklable):
+        pass
+    mnpo = MyNotPicklable()
+    with pytest.raises(TypeError):
+        pickle.dumps(mnpo)
