7e2c8f39c34c67956b8248d40a4936b5ff0fd814
Vlad Pavlov <vlpavlov@ieee.org>
2025-12-22 10:13:44 -0600

    CLN: comment out unused assertions in `post_init_metaclass`

--- src/pythagoras/_010_basic_portals/post_init_metaclass.py
+++ src/pythagoras/_010_basic_portals/post_init_metaclass.py
@@ -28,7 +28,7 @@ class PostInitMeta(ABCMeta):
         instance = super().__call__(*args, **kwargs)
         instance._post_init_hook()
         #TODO: remove/improve this check at some point in the future
-        if hasattr(instance, '_visited_portals'):
-            assert hasattr(instance, '_linked_portal_at_init')
-            assert len(instance._visited_portals) - int(instance._linked_portal_at_init is not None) == 0
+        # if hasattr(instance, '_visited_portals'):
+        #     assert hasattr(instance, '_linked_portal_at_init')
+        #     assert len(instance._visited_portals) - int(instance._linked_portal_at_init is not None) == 0
         return instance
