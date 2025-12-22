0e6a4e71ab04649f81508563d5ee03280c740650
Vlad Pavlov <vlpavlov@ieee.org>
2025-12-22 12:29:26 -0600

    CLN: remove commented-out assertions in `post_init_metaclass`

--- src/pythagoras/_010_basic_portals/post_init_metaclass.py
+++ src/pythagoras/_010_basic_portals/post_init_metaclass.py
@@ -27,8 +27,3 @@ class PostInitMeta(ABCMeta):
         """
         instance = super().__call__(*args, **kwargs)
         instance._post_init_hook()
-        #TODO: remove/improve this check at some point in the future
-        # if hasattr(instance, '_visited_portals'):
-        #     assert hasattr(instance, '_linked_portal_at_init')
-        #     assert len(instance._visited_portals) - int(instance._linked_portal_at_init is not None) == 0
-        return instance
