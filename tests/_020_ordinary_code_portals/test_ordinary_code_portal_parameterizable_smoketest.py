1ef6961da81089cba20caeb8bd400603ac94c106
Vlad Pavlov <vlpavlov@ieee.org>
2025-12-27 23:00:02 -0600

    TST: Polish tests for _020_ordinary_code_portals

--- tests/_020_ordinary_code_portals/test_ordinary_code_portal_parameterizable_smoketest.py
+++
@@ -1,9 +0,0 @@
-# from pythagoras._020_ordinary_code_portals import OrdinaryCodePortal
-# from pythagoras._010_basic_portals import _PortalTester
-# from parameterizable import smoketest_parameterizable_class
-#
-#
-# def test_basic_portal_get_params(tmpdir):
-#     with _PortalTester(OrdinaryCodePortal, root_dict = tmpdir) as t:
-#         smoketest_parameterizable_class(OrdinaryCodePortal)
-#
