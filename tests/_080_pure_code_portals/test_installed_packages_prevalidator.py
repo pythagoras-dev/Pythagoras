f6e8cf1a2b92d5481fc8af0f4a688b7a5a0d7a3e
Vlad Pavlov <vlpavlov@ieee.org>
2025-12-30 11:19:38 -0600

    TST: Extend exception handling in test_installed_packages_prevalidator.py

--- tests/_080_pure_code_portals/test_installed_packages_prevalidator.py
+++ tests/_080_pure_code_portals/test_installed_packages_prevalidator.py
@@ -31,5 +31,5 @@ def test_polars_package(tmpdir):
 
         uninstall_package(polars_package_name, use_uv=True)
 
-        with pytest.raises(ModuleNotFoundError):
+        with pytest.raises((ImportError, ModuleNotFoundError)):
             importlib.reload(package)
