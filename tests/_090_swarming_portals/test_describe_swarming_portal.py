d3a773259b3c91fa7dce74f2e466fd18f8368126
Vlad Pavlov <vlpavlov@ieee.org>
2025-12-30 11:20:47 -0600

    TST: Update test_describe_swarming_portal.py

--- tests/_090_swarming_portals/test_describe_swarming_portal.py
+++ tests/_090_swarming_portals/test_describe_swarming_portal.py
@@ -1,7 +1,7 @@
 from pythagoras import SwarmingPortal
 from pythagoras import _PortalTester
-from pythagoras._010_basic_portals.basic_portal_core_classes import _get_description_value_by_key
-from pythagoras._090_swarming_portals.swarming_portals import _BACKGROUND_WORKERS_TXT
+from pythagoras._010_basic_portals.portal_description_helpers import _get_description_value_by_key
+from pythagoras._090_swarming_portals.swarming_portals import _MAX_BACKGROUND_WORKERS_TXT
 
 
 def test_portal(tmpdir):
@@ -11,9 +11,9 @@ def test_portal(tmpdir):
             root_dict=tmpdir,
             max_n_workers=4)
         description = portal.describe()
-        assert description.shape == (12, 3)
-        assert _get_description_value_by_key(description
-            , _BACKGROUND_WORKERS_TXT) == portal.max_n_workers
+        assert description.shape == (16, 3)
+        assert _get_description_value_by_key(
+            description, _MAX_BACKGROUND_WORKERS_TXT) == portal.max_n_workers
 
 
 
