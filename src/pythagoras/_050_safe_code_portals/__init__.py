23c582738e415841435ee38d64f6cd2c27f37139
Vlad Pavlov <vlpavlov@ieee.org>
2025-12-30 21:28:19 -0600

    DOC: Expand and clarify docstring in `__init__.py` for SafeCodePortals and function wrappers

--- src/pythagoras/_050_safe_code_portals/__init__.py
+++ src/pythagoras/_050_safe_code_portals/__init__.py
@@ -1,15 +1,38 @@
-"""Classes and functions that allow safe execution of code.
+"""Safe execution infrastructure for Pythagoras (not yet implemented).
 
-The main classes in this sub-package are SafeCodePortal and SafeFn,
-which extend LoggingCodePortal and LoggingFn
-to provide safe execution capabilities for logging functions.
+This subpackage extends LoggingCodePortal with sandboxed execution capabilities,
+restricting functions from accessing external resources outside their local
+scope and the portal.
 
-SafeFn functions can't access (hence can't harm) any data/devices outside
-the function's local scope and the portal.
+Core Concepts
+-------------
+**SafeCodePortal**: Extends LoggingCodePortal with sandboxed execution.
+SafeFn functions registered with this portal will be prevented from accessing
+external filesystem, network, process state, etc. Currently behaves like
+LoggingCodePortal; sandboxing will be implemented via RestrictedPython.
 
-This functionality has not been implemented yet.
-It will be done soon by integrating https://pypi.org/project/RestrictedPython/
+**SafeFn**: Function wrapper created by the @safe decorator. Extends LoggingFn
+with execution isolation. Currently behaves like LoggingFn until sandboxing
+is implemented.
+
+**SafeFnCallSignature**: Unique identifier combining a SafeFn and its arguments,
+used to organize and retrieve execution artifacts in storage.
+
+Exports
+-------
+Portal and function classes:
+- SafeCodePortal: Portal with sandboxed execution (not yet enforced)
+- SafeFn: Wrapper for functions with execution isolation
+- SafeFnCallSignature: Identifier for a specific SafeFn call
+
+Decorator:
+- safe: Convert functions to SafeFn instances
+
+Note:
+    Sandboxing is not yet implemented. Integration with RestrictedPython
+    (https://pypi.org/project/RestrictedPython/) is planned.
 """
 
-from .safe_portal_core_classes import *
-from .safe_decorator import *
+from .safe_portal_core_classes import (
+    SafeCodePortal, SafeFn, SafeFnCallSignature)
+from .safe_decorator import safe
