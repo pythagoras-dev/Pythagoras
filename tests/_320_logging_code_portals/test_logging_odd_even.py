8da080a03483d8c53982cff2a310d893ce7a9ff1
Vlad Pavlov <vlpavlov@ieee.org>
2026-01-27 01:40:28 -0600

    CLN: standardize variable name from `l` to `t` in test modules

--- tests/_320_logging_code_portals/test_logging_odd_even.py
+++ tests/_320_logging_code_portals/test_logging_odd_even.py
@@ -45,7 +45,7 @@ def test_odd_even_two_decorators_logging(tmpdir):
             isEven = oldIsEven
             isOdd = oldIsOdd
 
-        assert l.portal.get_number_of_linked_functions() == 0
-        assert len([k for k in l.portal.global_value_store.keys() if k[2]== "bool"]) == 2
-        assert len([k for k in l.portal.global_value_store.keys() if k[2]== "int"]) == N
-        assert len([k for k in l.portal.global_value_store.keys() if k[2]== "packedkwargs_len_3"]) == N
+        assert t.portal.get_number_of_linked_functions() == 0
+        assert len([k for k in t.portal.global_value_store.keys() if k[2]== "bool"]) == 2
+        assert len([k for k in t.portal.global_value_store.keys() if k[2]== "int"]) == N
+        assert len([k for k in t.portal.global_value_store.keys() if k[2]== "packedkwargs_len_3"]) == N
