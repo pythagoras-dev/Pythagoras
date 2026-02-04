a4ee279e0165fa0622836df1eec56dff7aaa487b
Vlad Pavlov <vlpavlov@ieee.org>
2026-02-04 00:12:49 -0600

    DOC: Remove redundant comments and extra newlines in `test_portal_default_args.py`

--- tests/_210_basic_portals/test_portal_default_args.py
+++ tests/_210_basic_portals/test_portal_default_args.py
@@ -15,40 +15,33 @@ from pythagoras import (
 def test_default_args_behavior(tmpdir):
     """Verify accessor functions work correctly with default arguments."""
     _clear_all_portals()
-    # Create a portal
     p1 = BasicPortal(tmpdir)
-    
-    # Check default arg (should count p1)
+
     assert count_known_portals() == 1
     assert get_known_portals() == {p1}
-    
-    # Check explicit BasicPortal arg
+
     assert count_known_portals(BasicPortal) == 1
     assert get_known_portals(BasicPortal) == {p1}
-    
-    # Check that None is NOT allowed anymore (raises TypeError from validation)
+
     with pytest.raises(TypeError):
         count_known_portals(None)
-        
+
     with p1:
         assert count_active_portals() == 1
         assert count_active_portals(BasicPortal) == 1
         assert measure_active_portals_stack() == 1
         assert get_current_portal() == p1
-        
+
         with pytest.raises(TypeError):
             count_active_portals(None)
-            
-    # Check non-active/non-current
-    # Need another portal
+
     p2 = BasicPortal(str(tmpdir)+"/2")
-    
+
     with p1:
         assert p2 in get_nonactive_portals()
         assert p2 in get_nonactive_portals(BasicPortal)
         assert p2 in get_noncurrent_portals()
         assert p2 in get_noncurrent_portals(BasicPortal)
-        
-        with pytest.raises(TypeError):
-             get_nonactive_portals(None)
 
+        with pytest.raises(TypeError):
+            get_nonactive_portals(None)
